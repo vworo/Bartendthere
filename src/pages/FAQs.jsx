1535d520658c00c3b1a85a31a7b400aa95c6753c
vworo <victorworobec@gmail.com>
2023-09-04 23:09:07 +1000

    Added FAQs questions, to be styled.

--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -2,7 +2,72 @@ export default function FAQs() {
     return (
         <div>
             <h2>Frequently Asked Questions</h2>
-            <p>Frequently Asked Questions page coming soon.</p>
+
+            <h3>OUR SERVICES</h3>
+            <p>What we do</p>
+            <p>></p>
+            <p>How we work</p>
+            <p>> QR code</p>
+            <p>Can I have two areas of bar service?</p>
+            <p>></p>
+            <p>Can you do a cocktail or champagne on arrival?</p>
+            <p>></p>
+
+            <h3>BARTENDERS & WAITSTAFF</h3>
+            <p>How many staff will I need?</p>
+            <p>></p>
+            <p>Will the bartenders only serve from behind the bar?</p>
+            <p>></p>
+            <p>Can the bartenders offer table service?</p>
+            <p>></p>
+
+            <h3>GLASSWARE</h3>
+            <p>How many glasses do you provide?</p>
+            <p>></p>
+            <p>Do you have alternatives to glassware?</p>
+            <p>></p>
+
+            <h3>COCKTAILS (MENU)</h3>
+            <p>How do cocktails work with BYO Packages?</p>
+            <p>></p>
+            <p>Do you provide other alcohol alternatives?</p>
+            <p>></p>
+            <p>How does your custom menu work?</p>
+            <p>></p>
+
+            <h3>DRINK COOLING</h3>
+            <p>How do you keep the drinks cold?</p>
+            <p>></p>
+            <p>Do I need a cold room?</p>
+            <p>> </p>
+
+            <h3>LOGISTICS</h3>
+            <p>How far will you travel?</p>
+            <p>></p>
+            <p>Is set up and pack down time included?</p>
+            <p>></p>
+            <p>Will your setup be able to access my space?</p>
+            <p>></p>
+            <p>How much space do your setups need?</p>
+            <p>> We will work with the organiser to fit according to the floor plan.</p>
+            <p>Do you require power?</p>
+            <p>></p>
+            <p>Do you require access to water?</p>
+            <p>></p>
+            <p>What time do you arrive?</p>
+            <p>></p>
+            <p>What happens if it rains?</p>
+            <p>></p>
+
+            <h3>PAYMENTS & EXTENSIONS</h3>
+            <p>How much do you require as a deposit?</p>
+            <p>></p>
+            <p>When is the final payment due?</p>
+            <p>></p>
+            <p>Can I change or cancel my booking?</p>
+            <p>></p>
+            <p>Can I extend my service on the night?</p>
+            <p>></p>
         </div>
     )
 }
